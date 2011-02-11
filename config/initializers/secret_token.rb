140b71cd208d862e7745fb32b2fb963a8ed72b37
Levi Cook <levicook@gmail.com>
2011-02-10 17:27:38 -0700

    initial generaated application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FqlMe::Application.config.secret_token = '8d7003e6c28efffcb40ab905c4ff45c75c3746adeac49a89d96aea35b2feec32bfe24a4cfb1685e347d59e740b4e9a4727a96131bef264ba2157b8a74609b23b'
